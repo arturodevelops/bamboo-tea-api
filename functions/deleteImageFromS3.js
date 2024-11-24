3ae4c57e9bfad9348b8b727f93664eab41b8b725
Arturo Velaquez <8arturovelazquez@gmail.com>
2024-11-23 21:15:16 -0600

    actually delete the image from s3

--- functions/deleteImageFromS3.js
+++ functions/deleteImageFromS3.js
@@ -2,17 +2,21 @@ const { DeleteObjectCommand } = require('@aws-sdk/client-s3');
 const { s3 } = require('../models/AWS');
 
 const deleteImageFromS3 = async (fileName) => {
+  // Decode the file name to handle URL encoding
+  const decodedFileName = decodeURIComponent(fileName);
+
   const params = {
     Bucket: process.env.S3_BUCKET_NAME,
-    Key: `images/${fileName}`,
+    Key: `images/${decodedFileName}`,
   };
+
   try {
     await s3.send(new DeleteObjectCommand(params));
-    console.log(`Successfully deleted ${fileName} from S3.`);
+    console.log(`Successfully deleted ${decodedFileName} from S3.`);
   } catch (error) {
-    console.error(`Error deleting ${fileName}:`, error);
+    console.error(`Error deleting ${decodedFileName}:`, error);
     throw new Error('Failed to delete image from S3');
   }
 };
 
-module.exports = deleteImageFromS3
+module.exports = deleteImageFromS3;
